c0a4848b531366a4e0547f1a1aead1e3846c3d35
achingbrain <alex@achingbrain.net>
2014-03-06 11:25:25 +0000

    riffing

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
 
-var commander = require("commander"),
-	pkg = require(__dirname + "/package.json");
+var Container = require("wantsit").Container;
 
-commander
-	.version(pkg.version);
-
-commander
-	.command("list")
-	.description("List all running processes")
-	.action(function() {
-
-	});
-
-commander
-	.command("start [name]")
-	.description("Start a process")
-	.option("-u, --user", "The user to start a process as")
-	.option("-g, --group", "The group to start a process as")
-	.action(function() {
-
-	});
-
-commander
-	.command("stop [name]")
-	.description("Stop a process")
-	.action(function() {
-
-	});
-
-commander
-	.command("kill")
-	.description("Stop all processes and kill the daemon")
-	.action(function() {
-
-	});
-
-commander.parse(process.argv);
+var container = new Container();
+container.createAndRegister("cli", require(__dirname + "/lib/CLI"));
+container.createAndRegister("connection", require(__dirname + "/lib/api/Connection"), {
+	socket: "/tmp/boss"
+});
