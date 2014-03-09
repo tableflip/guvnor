fe4b6582e343a56338c43855a48223e302f74fb6
achingbrain <alex@achingbrain.net>
2014-03-09 22:49:05 +0000

    removed second daemon as it doesn't appear to solve the problem

--- boss.js
+++ boss.js
@@ -6,4 +6,3 @@ var config = require("rc")("boss", path.resolve(__dirname, ".bossrc"));
 var container = new Container();
 container.register("config", config);
 container.createAndRegister("boss", require("./lib/BossRPC"));
-container.createAndRegister("processStarter", require("./lib/DaemonStarter"), config.process.socket, path.resolve(__dirname, "process.js"));
--- process.js
+++
@@ -1,8 +0,0 @@
-var Container = require("wantsit").Container,
-	path = require("path");
-
-var config = require("rc")("boss", path.resolve(__dirname, ".bossrc"));
-
-var container = new Container();
-container.register("config", config);
-container.createAndRegister("process", require("./lib/ProcessRPC"));
