745fd1fdb67b591e5bbdf47d9cbc02687bcb8ab8
Alan Shaw <alan@freestyle-developments.co.uk>
2014-04-19 10:49:50 +0100

    Code style

--- test/ProcessInfoTest.js
+++ test/ProcessInfoTest.js
@@ -6,22 +6,22 @@ describe("ProcessInfo", function() {
 	it("should recover the process after crash recovery period", function(done) {
 		var processInfo = new ProcessInfo("test.js", sinon.stub(), {
 			crashRecoveryPeriod: 500
-		})
+		});
 
-		processInfo.restarts.should.equal(0)
-		processInfo.totalRestarts.should.equal(0)
+		processInfo.restarts.should.equal(0);
+		processInfo.totalRestarts.should.equal(0);
 
 		// Method under test
-		processInfo.stillCrashing()
+		processInfo.stillCrashing();
 
-		processInfo.restarts.should.equal(1)
-		processInfo.totalRestarts.should.equal(1)
+		processInfo.restarts.should.equal(1);
+		processInfo.totalRestarts.should.equal(1);
 
 		// After the crash recovery period, restarts should be reset
-		setTimeout(function () {
-			processInfo.restarts.should.equal(0)
-			processInfo.totalRestarts.should.equal(1)
-			done()
-		}, 1000)
+		setTimeout(function() {
+			processInfo.restarts.should.equal(0);
+			processInfo.totalRestarts.should.equal(1);
+			done();
+		}, 1000);
 	});
 });
