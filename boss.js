569d8b11ddec222904e1e2c6160caeb8e45aba3c
achingbrain <alex@achingbrain.net>
2014-06-30 14:31:26 +0100

    adds winston for more flexible logging

--- boss.js
+++ boss.js
@@ -1,8 +1,16 @@
 var Container = require('wantsit').Container,
-  path = require('path');
+  path = require('path'),
+  winston = require('winston')
 
 var config = require('rc')('boss', path.resolve(__dirname, '.bossrc'))
 
 var container = new Container()
 container.register('config', config)
+container.register('logger', new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      colorize: true
+    })
+  ]
+}))
 container.createAndRegister('boss', require('./lib/BossRPC'))
--- cli.js
+++ cli.js
@@ -1,9 +1,17 @@
 var Container = require('wantsit').Container,
-  path = require('path')
+  path = require('path'),
+  winston = require('winston')
 
 var config = require('rc')('boss', path.resolve(__dirname, '.bossrc'))
 
 var container = new Container()
 container.register('config', config)
+container.register('logger', new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      colorize: true
+    })
+  ]
+}))
 container.createAndRegister('cli', require('./lib/CLI'))
 container.createAndRegister('boss', require('./lib/DaemonStarter'), config.boss.socket, path.resolve(__dirname, 'boss.js'))
