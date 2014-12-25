a9cdfcabff937ffb51444ad474b0ddbfb47f9f4e
achingbrain <alex@achingbrain.net>
2014-12-25 10:38:09 +0000

    fixes #22 by not messing up the groupadd command and adds tests for group creation

--- test/lib/CLITest.js
+++ test/lib/CLITest.js
@@ -34,6 +34,20 @@ describe('CLI', function() {
     cli._os = {
       platform: sinon.stub()
     }
+    cli._prompt = {
+      start: sinon.stub(),
+      get: sinon.stub()
+    }
+    cli._execSync = {
+      exec: sinon.stub(),
+      run: sinon.stub()
+    }
+    cli._child_process = {
+      exec: sinon.stub()
+    }
+    cli._fs = {
+      appendFile: sinon.stub()
+    }
   })
 
   it('should parse list of arguments', function() {
@@ -92,5 +106,134 @@ describe('CLI', function() {
     cli._checkBossUser()
 
     expect(cli._logger.warn.called).to.be.false
+  })
+
+  it('should create a group on Linux', function(done) {
+    var groupadd = 'foo'
+    var code = 0
+
+    cli._config.boss.group = 'bar'
+
+    cli._posix.getgrnam.withArgs(cli._config.boss.group).throws(new Error('group id does not exist'))
+    cli._prompt.get.callsArg(1)
+
+    cli._execSync.exec.withArgs('which groupadd').returns({
+      stdout: groupadd
+    })
+
+    cli._execSync.run.withArgs(groupadd + ' ' + cli._config.boss.group).returns(code)
+
+    cli._checkBossGroup(function(error) {
+      expect(error).to.not.exist
+
+      done()
+    })
+  })
+
+  it('should complain if creating a group fails on Linux', function(done) {
+    var groupadd = 'foo'
+    var code = 1
+
+    cli._config.boss.group = 'bar'
+
+    cli._posix.getgrnam.withArgs(cli._config.boss.group).throws(new Error('group id does not exist'))
+    cli._prompt.get.callsArg(1)
+
+    cli._execSync.exec.withArgs('which groupadd').returns({
+      stdout: groupadd
+    })
+
+    cli._execSync.run.withArgs(groupadd + ' ' + cli._config.boss.group).returns(code)
+
+    cli._checkBossGroup(function(error) {
+      expect(error.message).to.equal('Automatically creating group bar failed, please create it manually')
+
+      done()
+    })
+  })
+
+  it('should create a group on Mac OS', function(done) {
+    var dscl = 'foo'
+    var groups = 'tomcat                                    257\n' +
+      'tty                                       4\n' +
+      'utmp                                      45\n' +
+      'wheel                                     0\n' +
+      'meh                                     500\n'
+
+    cli._config.boss.group = 'bar'
+
+    cli._posix.getgrnam.withArgs(cli._config.boss.group).throws(new Error('group id does not exist'))
+    cli._prompt.get.callsArg(1)
+
+    cli._execSync.exec.withArgs('which groupadd').returns({
+      stdout: ''
+    })
+    cli._execSync.exec.withArgs('which dscl').returns({
+      stdout: dscl
+    })
+
+    cli._child_process.exec.withArgs('foo . -list /Groups PrimaryGroupID').callsArgWith(1, undefined, groups)
+    cli._child_process.exec.withArgs('foo . create /Groups/bar').callsArg(1)
+    cli._child_process.exec.withArgs('foo . create /Groups/bar name bar').callsArg(1)
+    cli._child_process.exec.withArgs('foo . create /Groups/bar passwd "*"').callsArg(1)
+    cli._child_process.exec.withArgs('foo . create /Groups/bar gid 501').callsArg(1)
+    cli._fs.appendFile.withArgs('/etc/group', 'bar:*:501:\n').callsArg(2)
+
+    cli._checkBossGroup(function(error) {
+      expect(error).to.not.exist
+
+      done()
+    })
+  })
+
+  it('should complain if creating a group fails on Mac OS', function(done) {
+    var dscl = 'foo'
+    var groups = 'tomcat                                    257\n' +
+    'tty                                       4\n' +
+    'utmp                                      45\n' +
+    'wheel                                     0\n'
+
+    cli._config.boss.group = 'bar'
+
+    cli._posix.getgrnam.withArgs(cli._config.boss.group).throws(new Error('group id does not exist'))
+    cli._prompt.get.callsArg(1)
+
+    cli._execSync.exec.withArgs('which groupadd').returns({
+      stdout: ''
+    })
+    cli._execSync.exec.withArgs('which dscl').returns({
+      stdout: dscl
+    })
+
+    cli._child_process.exec.withArgs('foo . -list /Groups PrimaryGroupID').callsArgWith(1, undefined, groups)
+    cli._child_process.exec.withArgs('foo . create /Groups/bar').callsArg(1)
+    cli._child_process.exec.withArgs('foo . create /Groups/bar name bar').callsArg(1)
+    cli._child_process.exec.withArgs('foo . create /Groups/bar passwd "*"').callsArg(1)
+    cli._child_process.exec.withArgs('foo . create /Groups/bar gid 500').callsArgWith(1, new Error('Could not create group'))
+
+    cli._checkBossGroup(function(error) {
+      expect(error.message).to.equal('Could not create group')
+
+      done()
+    })
+  })
+
+  it('should complain dscl or groupadd are not present', function(done) {
+    cli._config.boss.group = 'foo'
+    cli._posix.getgrnam.withArgs(cli._config.boss.group).throws(new Error('group id does not exist'))
+    cli._prompt.get.callsArg(1)
+
+    cli._execSync.exec.withArgs('which groupadd').returns({
+      stdout: ''
+    })
+    cli._execSync.exec.withArgs('which dscl').returns({
+      stdout: ''
+    })
+
+    cli._checkBossGroup(function(error) {
+      expect(error.message).to.equal('Automatically creating group foo failed, please create it manually')
+
+      done()
+    })
   })
 })
