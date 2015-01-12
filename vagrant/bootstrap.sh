4a39839026e8bdc1ffbcdecf63b658cb55098276
achingbrain <alex@achingbrain.net>
2015-01-12 14:24:33 +0000

    do not auto-install

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+# Setup Node apt-get sources
 curl -sL https://deb.nodesource.com/setup | bash -
 
+# Update system and install node
+apt-get update -y
 apt-get install -y nodejs build-essential git
-
-npm install -g process-boss
 
 # Not required, but useful
 npm install -g node-inspector
+
+# Start boss in debug mode
+mkdir /etc/boss
+echo [debug] > /etc/boss/bossrc
+echo daemon = 5858 >> /etc/boss/bossrc
