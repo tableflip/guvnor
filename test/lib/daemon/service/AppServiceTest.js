1cb3f14c7c9a07698806d82ff125117bcccc9a33
achingbrain <alex@achingbrain.net>
2015-02-05 09:03:07 +0100

    added tests for deploying apps

--- test/lib/daemon/service/AppServiceTest.js
+++ test/lib/daemon/service/AppServiceTest.js
@@ -15,15 +15,120 @@ describe('AppService', function() {
       }
     }
     service._applicationStore = {
+      find: sinon.stub(),
       create: sinon.stub(),
-      warn: sinon.stub(),
-      error: sinon.stub(),
-      debug: sinon.stub(),
-      log: sinon.stub()
+      save: sinon.stub(),
+      remove: sinon.stub()
     }
   })
 
-  it('should deploy a project', function() {
+  it('should deploy a project', function(done) {
+    var name = 'foo'
+    var url = 'bar'
+    var user = 'baz'
+    var onOut = sinon.stub()
+    var onErr = sinon.stub()
+    var appInfo = {
+      clone: sinon.stub(),
+      installDependencies: sinon.stub()
+    }
 
+    appInfo.clone.callsArg(2)
+    appInfo.installDependencies.callsArg(2)
+
+    service._applicationStore.save.callsArg(0)
+
+    service._applicationStore.create.withArgs([{
+      name: name,
+      url: url,
+      user: user
+    }]).callsArgWith(1, undefined, appInfo)
+
+    var eventEmitted = false
+
+    service.once('app:installed', function() {
+      eventEmitted = true
+    })
+
+    service.deploy(name, url, user, onOut, onErr, function(error) {
+      expect(error).to.not.exist
+      expect(service._applicationStore.find.withArgs('name', name).called).to.be.true
+      expect(eventEmitted).to.be.true
+
+      done()
+    })
+  })
+
+  it('should not deploy a project with a duplicate name', function(done) {
+    var name = 'foo'
+    var url = 'bar'
+    var user = 'baz'
+    var onOut = sinon.stub()
+    var onErr = sinon.stub()
+
+    service._applicationStore.find.withArgs('name', name).returns({})
+
+    service.deploy(name, url, user, onOut, onErr, function(error) {
+      expect(error.message).to.contain('already exists')
+
+      done()
+    })
+  })
+
+  it('should pass error encountered when creating an application', function(done) {
+    var name = 'foo'
+    var url = 'bar'
+    var user = 'baz'
+    var onOut = sinon.stub()
+    var onErr = sinon.stub()
+    var appCreationError = new Error('urk!')
+
+    service._applicationStore.create.withArgs([{
+      name: name,
+      url: url,
+      user: user
+    }]).callsArgWith(1, appCreationError)
+
+    service.deploy(name, url, user, onOut, onErr, function(error) {
+      expect(error).to.equal(appCreationError)
+
+      done()
+    })
+  })
+
+  it('should remove repo when cloning or installing fails', function(done) {
+    var name = 'foo'
+    var url = 'bar'
+    var user = 'baz'
+    var onOut = sinon.stub()
+    var onErr = sinon.stub()
+    var appInfo = {
+      clone: sinon.stub(),
+      installDependencies: sinon.stub(),
+      remove: sinon.stub()
+    }
+    var installError = new Error('urk!')
+
+    appInfo.clone.callsArg(2)
+    appInfo.installDependencies.callsArgWith(2, installError)
+    appInfo.remove.callsArg(0)
+
+    service._applicationStore.create.withArgs([{
+      name: name,
+      url: url,
+      user: user
+    }]).callsArgWith(1, undefined, appInfo)
+
+    var eventEmitted = false
+
+    service.once('app:installed', function() {
+      eventEmitted = true
+    })
+
+    service.deploy(name, url, user, onOut, onErr, function(error) {
+      expect(error).to.equal(installError)
+
+      done()
+    })
   })
 })
