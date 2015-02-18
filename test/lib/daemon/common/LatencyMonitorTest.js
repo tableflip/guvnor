1db80d83cdbabe1c0f375edf86a1825d71eac4ae
achingbrain <alex@achingbrain.net>
2015-02-18 10:41:46 +0000

    correct negative latency measurements and add test

---
+++ test/lib/daemon/common/LatencyMonitorTest.js
@@ -0,0 +1,34 @@
+var sinon = require('sinon'),
+  expect = require('chai').expect,
+  LatencyMonitor = require('../../../../lib/daemon/common/LatencyMonitor')
+
+describe('LatencyMonitor', function() {
+  var monitor, lag
+
+  beforeEach(function() {
+    lag = sinon.stub()
+
+    monitor = new LatencyMonitor()
+    monitor._lag = sinon.stub()
+
+    monitor._lag.withArgs(1000).returns(lag)
+
+    monitor.afterPropertiesSet()
+  })
+
+  it('should report latency', function() {
+    var latency = 5
+
+    lag.returns(latency)
+
+    expect(monitor.latency).to.equal(latency)
+  })
+
+  it('should correct spurious latency', function() {
+    var latency = -10
+
+    lag.returns(latency)
+
+    expect(monitor.latency).to.equal(0)
+  })
+})
