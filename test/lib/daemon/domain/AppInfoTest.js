332452dc17c2710dbcdfd84b07f9719d095b1fc3
achingbrain <alex@achingbrain.net>
2015-03-14 22:10:28 +0100

    appinfo tests

--- test/lib/daemon/domain/AppInfoTest.js
+++ test/lib/daemon/domain/AppInfoTest.js
@@ -22,8 +22,39 @@ describe('AppInfo', function () {
     appInfo._userDetailsFactory = {
       create: sinon.stub()
     }
+    appInfo._fs = {
+      exists: sinon.stub()
+    }
+  })
 
-    appInfo.afterPropertiesSet()
+  it('should create a user details object', function (done) {
+    var userDetails = {
+      name: 'foo'
+
+    }
+    var appDir = 'bar'
+
+    appInfo._fileSystem.getAppDir.returns(appDir)
+    appInfo._userDetailsFactory.create.withArgs([appInfo.user]).callsArgWith(1, undefined, userDetails)
+
+    appInfo.afterPropertiesSet(function (error) {
+      expect(error).not.to.exist
+      expect(appInfo._user).to.equal(userDetails)
+      expect(appInfo.path).to.equal(appDir + '/' + appInfo.id)
+
+      done()
+    })
+  })
+
+  it('should propagate error creating a user details object', function (done) {
+    var error = new Error('Urk!')
+    appInfo._userDetailsFactory.create.withArgs([appInfo.user]).callsArgWith(1, error)
+
+    appInfo.afterPropertiesSet(function (er) {
+      expect(er).to.equal(error)
+
+      done()
+    })
   })
 
   it('should clone a repo', function (done) {
@@ -96,5 +127,77 @@ describe('AppInfo', function () {
       if (error.message.indexOf('user') == -1)
         throw error
     }
+  })
+
+  it('should list refs', function (done) {
+    appInfo.listRefs(function (error, refs) {
+      expect(error).not.to.exist
+      expect(refs[1].name).to.equal('refs/tags/foo')
+      expect(refs[2].name).to.equal('refs/tags/bar')
+
+      done()
+    })
+
+    expect(appInfo._commandLine.git.callCount).to.equal(1)
+    expect(appInfo._commandLine.git.calledWith(['show-ref'])).to.be.true
+
+    appInfo._commandLine.git.getCall(0).args[3]('c044400c5d42ec977721f267e304e4a3ca5d704e refs/tags/v3.2.0\n')
+    appInfo._commandLine.git.getCall(0).args[3]('c044400c5d42ec977721f267e304e4a3ca5d704e refs/tags/foo\n')
+    appInfo._commandLine.git.getCall(0).args[3]('c044400c5d42ec977721f267e304e4a3ca5d704e refs/tags/bar\n')
+    appInfo._commandLine.git.getCall(0).args[3]('c044400c5d42ec977721f267e304e4a3ca5d704e refs/tags/v3.2.0\n')
+
+    appInfo._commandLine.git.getCall(0).args[6]()
+  })
+
+  it('should report the current ref', function (done) {
+    var currentRef = 'foo'
+
+    appInfo.currentRef(function (error, ref) {
+      expect(error).not.to.exist
+      expect(ref).to.equal(currentRef)
+
+      done()
+    })
+
+    expect(appInfo._commandLine.git.callCount).to.equal(1)
+    expect(appInfo._commandLine.git.calledWith(['rev-parse', '--abbrev-ref', 'HEAD'])).to.be.true
+
+    appInfo._commandLine.git.getCall(0).args[3](currentRef + '\n')
+    appInfo._commandLine.git.getCall(0).args[6]()
+  })
+
+  it('should update refs', function (done) {
+    appInfo._commandLine.git.withArgs(['reset', '--hard', 'HEAD']).callsArg(6)
+    appInfo._commandLine.git.withArgs(['clean', '-d', '-f']).callsArg(6)
+    appInfo._commandLine.git.withArgs(['pull']).callsArg(6)
+    appInfo.listRefs = sinon.stub()
+    appInfo.listRefs.callsArgWith(0, undefined, 'refs')
+
+    appInfo.updateRefs(sinon.stub(), sinon.stub(), function (error, updatedApp, refs) {
+      expect(error).not.to.exist
+      expect(updatedApp).to.equal(appInfo)
+      expect(refs).to.equal('refs')
+
+      done()
+    })
+  })
+
+  it('should not install dependencies when no package.json exists', function (done) {
+    appInfo.path = 'foo'
+
+    appInfo._fs.exists.withArgs(appInfo.path + '/package.json').callsArgWith(1, false)
+
+    appInfo.installDependencies(null, null, done)
+  })
+
+  it('should install dependencies', function (done) {
+    appInfo.path = 'foo'
+
+    appInfo._fs.exists.withArgs(appInfo.path + '/package.json').callsArgWith(1, true)
+
+    appInfo._rimraf.withArgs(appInfo.path + '/node_modules').callsArg(1)
+    appInfo._commandLine.npm.callsArg(6)
+
+    appInfo.installDependencies(sinon.stub(), sinon.stub(), done)
   })
 })
