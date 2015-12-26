8aeb72045850bd5c527ebb47b5079d6a78617452
achingbrain <alex@achingbrain.net>
2015-12-26 12:49:26 +0000

    showing process logs

--- test/fixtures/intermittently-crashy.js
+++ test/fixtures/intermittently-crashy.js
@@ -1,7 +1,4 @@
-console.info('hello world')
-console.error('err world')
 // crash every 10-20 seconds - e.g. not often enough to cause the process to be aborted
 setTimeout(function () {
-  console.info('urk!')
   throw new Error('I die!')
 }, ~~(Math.random() * 10000) + 10000)
